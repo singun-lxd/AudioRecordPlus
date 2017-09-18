cfe92122c06d88f10eedc6cebbfe4fecb2a20328
singun-lxd <liuxindian@gmail.com>
2017-09-18 22:06:53 +0800

    * fix compile error

--- app/src/main/cpp/agc/agc_wrapper.cc
+++ app/src/main/cpp/agc/agc_wrapper.cc
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "agc_wrapper.h"
 #include "gain_control.h"
 
--- app/src/main/cpp/ns/ns_wrapper.cc
+++ app/src/main/cpp/ns/ns_wrapper.cc
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "noise_suppression.h"
 #include "ns_wrapper.h"
 
